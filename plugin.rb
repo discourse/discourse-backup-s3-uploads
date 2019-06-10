c179642526d8803789921523bce3954bed58d7b9
Guo Xiang Tan <tgx_world@hotmail.com>
2019-06-10 11:23:05 +0800

    Initial commit.

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# name: discourse-backup-s3-uploads
+# about: Adds a callback to backup s3 uploads to another bucket.
+# version: 0.0.1
+# url: https://github.com/discourse/discourse-backup-s3-uploads
